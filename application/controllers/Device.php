7219d626b0a2a7b32e8b74834209cb3b422e6e62
Narongsak Mala <narongsak.mala@gmail.com>
2018-08-19 10:58:57 +0000

    fix model device

--- application/controllers/Device.php
+++ application/controllers/Device.php
@@ -1,12 +1,11 @@
 <?php
 class Device extends CI_Controller{
 	public function __construct(){
-		$this->load->model(
-			"M_device"
-		);
+		parent::__construct();	
+		$this->load->model('M_device');
 	}
 	public function status($box_id=null, $id=null){
-		$status = $this->M_device->get_devices();
+		$status = $this->M_device->get_devices($box_id, $id);
 		$response['result_code'] = 0;
 
 		if( $status['status'] == 'OK' )
--- application/models/M_device.php
+++ application/models/M_device.php
@@ -4,16 +4,15 @@ class M_device extends CI_Model{
         $this->load->database();
     }
     public function get_devices($id_box, $id){
-        $result = [];
-        $result["status"] = "OK";
-        if( !(is_empty($id_box) && !is_emtry($id)) 
+        $result['status'] = "OK";
+        if( !empty($id_box) and !empty($id) ) {
             $query = $this->db->get_where("tbl_device", array('id_box' => $id_box, 'id'=> $id));
-            $result["data"] = $query->result_array();
-        }else if ( !is_empty($id_box) ){
+            $result['data'] = $query->result_array();
+        }else if ( !empty($id_box) ){
             $query = $this->db->get_where("tbl_device", array('id_box' => $id_box ));
-            $result["data"] = $query->result_array();
+            $result['data'] = $query->result_array();
         }else{
-            $result["status"] = "Failed";
+            $result['status'] = "Failed";
         }
         return $result;
     }
