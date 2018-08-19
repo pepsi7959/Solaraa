84d87be71023467d6cbbd4fc8196cc68fefb5354
pepsi7959 <pepsi_kmutnb@windowslive.com>
2018-08-19 17:24:09 +0700

    support status inquiry

--- application/controllers/Device.php
+++ application/controllers/Device.php
@@ -1,9 +1,21 @@
 <?php
 class Device extends CI_Controller{
 	public function __construct(){
+		$this->load->model(
+			"M_device"
+		);
 	}
-	public function status(){
-		echo "hello world";			
+	public function status($box_id=null, $id=null){
+		$status = $this->M_device->get_devices();
+		$response['result_code'] = 0;
+
+		if( $status['status'] == 'OK' )
+			$response['data'] = $status["data"];
+		else{
+			$response['result_code'] = 0x01;
+			$response['data'] = '';
+		}
+		echo json_encode($response);
 	}
 }
 ?>
--- application/models/M_device.php
+++ application/models/M_device.php
@@ -1,21 +1,25 @@
 <?php 
 class M_device extends CI_Model{
-	public function __construct(){
-		$this->load->database();
-	}
-	public function get_devices($id_box, $id){
-		if( !(is_empty($id_box) && !is_emtry($id)) 
-			$query = $this->db->get_where("tbl_device", array('id_box' => $id_box, 'id'=> $id));
-		}else if ( !is_empty($id_box) ){
-			$query = $this->db->get_where("tbl_device", array('id_box' => $id_box ));
-		}else{
-			return null;
-		}
-		return $query->result_array();
-	}
+    public function __construct(){
+        $this->load->database();
+    }
+    public function get_devices($id_box, $id){
+        $result = [];
+        $result["status"] = "OK";
+        if( !(is_empty($id_box) && !is_emtry($id)) 
+            $query = $this->db->get_where("tbl_device", array('id_box' => $id_box, 'id'=> $id));
+            $result["data"] = $query->result_array();
+        }else if ( !is_empty($id_box) ){
+            $query = $this->db->get_where("tbl_device", array('id_box' => $id_box ));
+            $result["data"] = $query->result_array();
+        }else{
+            $result["status"] = "Failed";
+        }
+        return $result;
+    }
 
-	public function set_device($id, $status){
-	
-	}	
+    public function set_device($id, $status){
+    
+    }	
 }
 ?>
